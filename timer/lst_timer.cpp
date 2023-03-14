f6b333655852c17327f1c57dfad1179633ddbd53
Wild Pointer <15146245289@163.com>
2023-03-14 15:32:25 +0800

    20230314learning

--- timer/lst_timer.cpp
+++ timer/lst_timer.cpp
@@ -99,7 +99,7 @@ void sort_timer_lst::tick()
     {
         return;
     }
-    
+
     time_t cur = time(NULL);
     util_timer *tmp = head;
     while (tmp)
@@ -150,7 +150,7 @@ void Utils::init(int timeslot)
     m_TIMESLOT = timeslot;
 }
 
-//对文件描述符设置非阻塞
+// 对文件描述符设置非阻塞
 int Utils::setnonblocking(int fd)
 {
     int old_option = fcntl(fd, F_GETFL);
@@ -159,7 +159,7 @@ int Utils::setnonblocking(int fd)
     return old_option;
 }
 
-//将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
+// 将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
 void Utils::addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
 {
     epoll_event event;
@@ -176,29 +176,36 @@ void Utils::addfd(int epollfd, int fd, bool one_shot, int TRIGMode)
     setnonblocking(fd);
 }
 
-//信号处理函数
+// 信号处理函数
 void Utils::sig_handler(int sig)
 {
-    //为保证函数的可重入性，保留原来的errno
+    // 为保证函数的可重入性，保留原来的errno
+    // 可重入性表示中断后再次进入该函数，环境变量与之前相同，不会丢失数据
     int save_errno = errno;
     int msg = sig;
+    // 将信号值从管道写端写入，传输字符类型，而非整型
     send(u_pipefd[1], (char *)&msg, 1, 0);
+    // 将原来的errno赋值为当前的errno
     errno = save_errno;
 }
 
-//设置信号函数
+// 设置信号函数
 void Utils::addsig(int sig, void(handler)(int), bool restart)
 {
+    // 创建sigaction结构体变量
     struct sigaction sa;
     memset(&sa, '\0', sizeof(sa));
+    // 信号处理函数中仅仅发送信号值，不做对应逻辑处理
     sa.sa_handler = handler;
     if (restart)
         sa.sa_flags |= SA_RESTART;
+    // 将所有信号添加到信号集中
     sigfillset(&sa.sa_mask);
+    // 执行sigaction函数
     assert(sigaction(sig, &sa, NULL) != -1);
 }
 
-//定时处理任务，重新定时以不断触发SIGALRM信号
+// 定时处理任务，重新定时以不断触发SIGALRM信号
 void Utils::timer_handler()
 {
     m_timer_lst.tick();
@@ -215,10 +222,14 @@ int *Utils::u_pipefd = 0;
 int Utils::u_epollfd = 0;
 
 class Utils;
+// 定时器回调函数
 void cb_func(client_data *user_data)
 {
+    // 删除非活动连接在socket上的注册事件
     epoll_ctl(Utils::u_epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
     assert(user_data);
+    // 关闭文件描述符
     close(user_data->sockfd);
+    // 减少连接数
     http_conn::m_user_count--;
 }
--- timer/lst_timer.h
+++ timer/lst_timer.h
@@ -24,12 +24,17 @@
 #include <time.h>
 #include "../log/log.h"
 
+// 连接资源结构体成员需要用到定时器类
+// 需要前向声明
 class util_timer;
-
+// 连接资源
 struct client_data
 {
+    // 客户端socket地址
     sockaddr_in address;
+    // socket文件描述符
     int sockfd;
+    // 定时器
     util_timer *timer;
 };
 
@@ -39,11 +44,15 @@ public:
     util_timer() : prev(NULL), next(NULL) {}
 
 public:
+    // 超时时间
     time_t expire;
-    
-    void (* cb_func)(client_data *);
+    // 回调函数
+    void (*cb_func)(client_data *);
+    // 连接资源
     client_data *user_data;
+    // 前向定时器
     util_timer *prev;
+    // 后继定时器
     util_timer *next;
 };
 
@@ -73,19 +82,19 @@ public:
 
     void init(int timeslot);
 
-    //对文件描述符设置非阻塞
+    // 对文件描述符设置非阻塞
     int setnonblocking(int fd);
 
-    //将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
+    // 将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
     void addfd(int epollfd, int fd, bool one_shot, int TRIGMode);
 
-    //信号处理函数
+    // 信号处理函数
     static void sig_handler(int sig);
 
-    //设置信号函数
+    // 设置信号函数
     void addsig(int sig, void(handler)(int), bool restart = true);
 
-    //定时处理任务，重新定时以不断触发SIGALRM信号
+    // 定时处理任务，重新定时以不断触发SIGALRM信号
     void timer_handler();
 
     void show_error(int connfd, const char *info);
